37719d46a50cfa3a20c660d36dcddac51082e649
ayguillo <ayguillo@student.42.fr>
2021-07-05 15:00:02 +0200

    [CORREC]find neigbors

--- algorithm/findexplo.go
+++ algorithm/findexplo.go
@@ -1,10 +1,16 @@
 package algorithm
 
 import (
+	"fmt"
+	d "gomoku/display"
 	s "gomoku/structures"
 )
 
 func inNeighbors(ctx *s.SContext, vertex s.SVertex, array_neigbors []s.SVertex) []s.SVertex {
+	if (vertex.Y < 0 || vertex.Y >= 19) || (vertex.X < 0 || vertex.X >= 19) {
+		return array_neigbors
+	}
+	fmt.Println(ctx.Goban[vertex.Y][vertex.X], vertex.Y, vertex.X)
 	if ctx.Goban[vertex.Y][vertex.X] != 0 {
 		return array_neigbors
 	}
@@ -35,12 +41,22 @@ func FindNeighbors(ctx *s.SContext, case_x int, case_y int, visu *s.SVisu) {
 	array = inNeighbors(ctx, s.SVertex{X: case_x + 1, Y: case_y}, array)
 	array = inNeighbors(ctx, s.SVertex{X: case_x - 1, Y: case_y}, array)
 	ctx.CasesNonNull[cases_vertex] = array
-	// color := [4]uint8{83, 51, 237, 1}
-	// for _, elem := range ctx.CasesNonNull {
-	// 	// fmt.Println("Key", key)
-	// 	for _, onecase := range elem {
-	// 		// fmt.Println("\tarray", onecase)
-	// 		d.TraceStone(float64(onecase.X), float64(onecase.Y), ctx, visu, color, false)
-	// 	}
-	// }
+	color := [4]uint8{83, 51, 237, 1}
+	for key, elem := range ctx.CasesNonNull {
+		fmt.Println("Key", key)
+		len_elem, index := len(elem), 0
+		for index < len_elem {
+			fmt.Println("\tarray", elem[index])
+			if ctx.Goban[elem[index].Y][elem[index].X] != 0 {
+				elem[index] = elem[len(elem)-1]
+				elem[len(elem)-1] = s.SVertex{X: 0, Y: 0}
+				elem = elem[:len(elem)-1]
+				len_elem--
+			} else {
+				d.TraceStone(float64(elem[index].X), float64(elem[index].Y), ctx, visu, color, false)
+				index++
+			}
+		}
+	}
+	fmt.Println("\n")
 }
