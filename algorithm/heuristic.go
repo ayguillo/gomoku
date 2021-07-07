646c6543ff1f5ccea3d1825d7e7423a93970f4da
ayguillo <ayguillo@student.42.fr>
2021-07-07 17:16:04 +0200

    [ADD]heuristic files

---
+++ algorithm/heuristic.go
@@ -0,0 +1,43 @@
+package algorithm
+
+import (
+	s "gomoku/structures"
+)
+
+func conditionAlignHeuristic(ctx s.SContext, case_x int, case_y int) int {
+	capturePlayer, nbCapture := 0, 0
+	if ctx.CurrentPlayer == 1 {
+		capturePlayer = 2
+		nbCapture = ctx.NbCaptureP2
+	} else {
+		capturePlayer = 1
+		nbCapture = ctx.NbCaptureP1
+	}
+	horizontal := horizontalHeuristicAlign(ctx, case_x, case_y, capturePlayer, nbCapture)
+	vertical := verticalHeuristicAlign(ctx, case_x, case_y, capturePlayer, nbCapture)
+	diagonal := diagonalHeuristicAlign(ctx, case_x, case_y, capturePlayer, nbCapture)
+	return horizontal + vertical + diagonal
+	return horizontal
+}
+
+func captureHeuristic(ctx s.SContext, case_x int, case_y int) int {
+	var capture uint8
+	score_capture := 0
+	if ctx.CurrentPlayer == 1 {
+		capture = 2
+		score_capture = 10 * ctx.NbCaptureP1
+	} else {
+		capture = 1
+		score_capture = 10 * ctx.NbCaptureP2
+	}
+	horizontal := horizontalCaptureHeuristic(ctx, case_x, case_y, capture, score_capture)
+	vertical := verticalCaptureHeuristic(ctx, case_x, case_y, capture, score_capture)
+	diagonal := diagonalCaptureHeuristic(ctx, case_x, case_y, capture, score_capture)
+	return horizontal + vertical + diagonal
+}
+
+func Heuristic(ctx s.SContext, case_x int, case_y int) int {
+	align := conditionAlignHeuristic(ctx, case_x, case_y)
+	capture := captureHeuristic(ctx, case_x, case_y)
+	return align + capture
+}
---
+++ algorithm/heuristicCapture.go
@@ -0,0 +1,199 @@
+package algorithm
+
+import (
+	s "gomoku/structures"
+)
+
+func diagLeftCaptureHeuristic(ctx s.SContext, case_x int, case_y int, capture uint8, score_capture int) int {
+	count_stone := 0
+	current_case_x := case_x + 1
+	current_case_y := case_y + 1
+	ret_value := 0
+	for current_case_x < int(ctx.NSize) && current_case_y < int(ctx.NSize) {
+		if ctx.Goban[current_case_y][current_case_x] == s.Tnumber(capture) {
+			count_stone++
+			current_case_x++
+			current_case_y++
+		} else {
+			break
+		}
+	}
+	if count_stone == 2 && current_case_x < int(ctx.NSize) && current_case_y < int(ctx.NSize) {
+		if ctx.Goban[current_case_y][current_case_x] == s.Tnumber(ctx.CurrentPlayer) {
+			// addToNeigbors(ctx, current_case_x-1, current_case_y-1, current_case_x, current_case_y, visu) A ajouter sans visu
+			// addToNeigbors(ctx, current_case_x-2, current_case_y-2, case_x, case_y, visu) A ajouter sans visu
+			ret_value += 20
+		} else if ctx.Goban[current_case_y][current_case_x] == 0 {
+			ret_value += 10
+		}
+	}
+	count_stone = 0
+	current_case_x = case_x - 1
+	current_case_y = case_y - 1
+	for current_case_x >= 0 && current_case_y >= 0 {
+		if ctx.Goban[current_case_y][current_case_x] == s.Tnumber(capture) {
+			count_stone++
+			current_case_x--
+			current_case_y--
+		} else {
+			break
+		}
+	}
+	if count_stone == 2 && current_case_x >= 0 && current_case_y >= 0 {
+		if ctx.Goban[current_case_y][current_case_x] == s.Tnumber(ctx.CurrentPlayer) {
+			// addToNeigbors(ctx, current_case_x+1, current_case_y+1, current_case_x, current_case_y, visu)  A ajouter sans visu
+			// addToNeigbors(ctx, current_case_x+2, current_case_y+2, case_x, case_y, visu) A ajouter sans
+			ret_value += 20
+		} else if ctx.Goban[current_case_y][current_case_x] == 0 {
+			ret_value += 10
+		}
+	}
+	if ret_value != 0 {
+		ret_value += score_capture
+	}
+	return ret_value
+}
+
+func diagRightCaptureHeuristic(ctx s.SContext, case_x int, case_y int, capture uint8, score_capture int) int {
+	count_stone := 0
+	current_case_x := case_x + 1
+	current_case_y := case_y - 1
+	ret_value := 0
+	for current_case_x < int(ctx.NSize) && current_case_y >= 0 {
+		if ctx.Goban[current_case_y][current_case_x] == s.Tnumber(capture) {
+			count_stone++
+			current_case_x++
+			current_case_y--
+		} else {
+			break
+		}
+	}
+	if count_stone == 2 && current_case_x < int(ctx.NSize) && current_case_y >= 0 {
+		if ctx.Goban[current_case_y][current_case_x] == s.Tnumber(ctx.CurrentPlayer) {
+			// addToNeigbors(ctx, current_case_x-1, current_case_y+1, current_case_x, current_case_y, visu) A ajouter sans visu
+			// addToNeigbors(ctx, current_case_x-2, current_case_y+2, case_x, case_y, visu) A ajouter sans visu
+			ret_value += 20
+		} else if ctx.Goban[current_case_y][current_case_x] == 0 {
+			ret_value += 10
+		}
+	}
+	count_stone = 0
+	current_case_x = case_x - 1
+	current_case_y = case_y + 1
+	for current_case_x >= 0 && current_case_y < int(ctx.NSize) {
+		if ctx.Goban[current_case_y][current_case_x] == s.Tnumber(capture) {
+			count_stone++
+			current_case_x--
+			current_case_y++
+		} else {
+			break
+		}
+	}
+	if count_stone == 2 && current_case_x < int(ctx.NSize) && current_case_y >= 0 {
+		if ctx.Goban[current_case_y][current_case_x] == s.Tnumber(ctx.CurrentPlayer) {
+			// addToNeigbors(ctx, current_case_x+1, current_case_y-1, current_case_x, current_case_y, visu) A ajouter sans visu
+			// addToNeigbors(ctx, current_case_x+2, current_case_y-2, case_x, case_y, visu) A ajouter sans visu
+			ret_value += 20
+		} else if ctx.Goban[current_case_y][current_case_x] == 0 {
+			ret_value += 10
+		}
+	}
+	if ret_value != 0 {
+		ret_value += score_capture
+	}
+	return ret_value
+}
+
+func diagonalCaptureHeuristic(ctx s.SContext, case_x int, case_y int, capture uint8, score_capture int) int {
+	left := diagLeftCaptureHeuristic(ctx, case_x, case_y, capture, score_capture)
+	right := diagRightCaptureHeuristic(ctx, case_x, case_y, capture, score_capture)
+	return left + right
+}
+
+func horizontalCaptureHeuristic(ctx s.SContext, case_x int, case_y int, capture uint8, score_capture int) int {
+	count_stone := 0
+	current_case := case_x + 1
+	ret_value := 0
+	for ; current_case < int(ctx.NSize); current_case++ {
+		if ctx.Goban[case_y][current_case] == s.Tnumber(capture) {
+			count_stone++
+		} else {
+			break
+		}
+	}
+	if count_stone == 2 && current_case < int(ctx.NSize) {
+		if ctx.Goban[case_y][current_case] == s.Tnumber(ctx.CurrentPlayer) {
+			// addToNeigbors(ctx, current_case-1, case_y, case_x, case_y, visu) A ajouter sans visu
+			// addToNeigbors(ctx, current_case-2, case_y, current_case+1, case_y, visu) A ajouter sans visu
+			ret_value += 20
+		} else if ctx.Goban[case_y][current_case] == 0 {
+			ret_value += 10
+		}
+	}
+	count_stone = 0
+	current_case = case_x - 1
+	for ; current_case >= 0; current_case-- {
+		if ctx.Goban[case_y][current_case] == s.Tnumber(capture) {
+			count_stone++
+		} else {
+			break
+		}
+	}
+	if count_stone == 2 && current_case >= 0 {
+		if ctx.Goban[case_y][current_case] == s.Tnumber(ctx.CurrentPlayer) {
+			// addToNeigbors(ctx, current_case+1, case_y, current_case, case_y, visu) A ajouter sans visu
+			// addToNeigbors(ctx, current_case+2, case_y, case_x, case_y, visu) A ajouter sans visu
+			ret_value += 20
+		} else if ctx.Goban[case_y][current_case] == 0 {
+			ret_value += 10
+		}
+	}
+	if ret_value != 0 {
+		ret_value += score_capture
+	}
+	return ret_value
+}
+
+func verticalCaptureHeuristic(ctx s.SContext, case_x int, case_y int, capture uint8, score_capture int) int {
+	count_stone := 0
+	current_case := case_y + 1
+	ret_value := 0
+	for ; current_case < int(ctx.NSize); current_case++ {
+		if ctx.Goban[current_case][case_x] == s.Tnumber(capture) {
+			count_stone++
+		} else {
+			break
+		}
+	}
+	if count_stone == 2 && current_case < int(ctx.NSize) {
+		if ctx.Goban[current_case][case_x] == s.Tnumber(ctx.CurrentPlayer) {
+			// addToNeigbors(ctx, case_x, current_case-1, case_x, case_y, visu) A ajouter sans visu
+			// addToNeigbors(ctx, case_x, current_case-2, case_x, current_case+1, visu) A ajouter sans
+			ret_value += 20
+		} else if ctx.Goban[current_case][case_x] == 0 {
+			ret_value += 10
+		}
+	}
+	count_stone = 0
+	current_case = case_y - 1
+	for ; current_case >= 0; current_case-- {
+		if ctx.Goban[current_case][case_x] == s.Tnumber(capture) {
+			count_stone++
+		} else {
+			break
+		}
+	}
+	if count_stone == 2 && current_case >= 0 {
+		if ctx.Goban[current_case][case_x] == s.Tnumber(ctx.CurrentPlayer) {
+			// addToNeigbors(ctx, case_x, current_case+1, case_x, current_case, visu) A ajouter sans visu
+			// addToNeigbors(ctx, case_x, current_case+2, case_x, case_y, visu) A ajouter sans visu
+			ret_value += 20
+		} else if ctx.Goban[current_case][case_x] == 0 {
+			ret_value += 10
+		}
+	}
+	if ret_value != 0 {
+		ret_value += score_capture
+	}
+	return ret_value
+}
